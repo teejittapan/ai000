7f375b49edc69c65835d2c5c8b62db11686e02df
teejittapan <146495749+teejittapan@users.noreply.github.com>
2024-03-10 22:03:52 +0700

    Update index.ts app title

--- config/index.ts
+++ config/index.ts
@@ -3,8 +3,8 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'AI 01 App',
-  description: 'TikTok Scripter',
+  title: 'TikTok Scripter',
+  description: 'AI 01 App',
   copyright: '',
   privacy_policy: '',
   default_language: 'en-US',
